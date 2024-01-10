09b5029be52cc598e498790e598ca4f27355950e
Pepa Labrin <catu.leibrin@gmail.com>
2024-01-10 13:07:00 -0300

    added const into the js

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
 
-let nombre = prompt("¡Bienvenido al VerdurasShop! Ingresa tu nombre a continuación:")
+const palta = {
+    nombre: "1 Kilo de Palta",
+    precio: 4000,
+}
+
+const pepino = {
+    nombre: "1 Kilo de Pepino",
+    precio: 3000,
+}
+
+const tomate = {
+    nombre: "1 Kilo de Tomate",
+    precio: 3500,
+}
+
+const porotos = {
+    nombre: "1 Kilo de Porotos",
+    precio: 2100,
+}
+
+let nombre = prompt("¡Bienvenido al VerdurasShop! Ingresa tu nombre a continuación:");
+let afiliacion = prompt("¿Estás afiliado?");
